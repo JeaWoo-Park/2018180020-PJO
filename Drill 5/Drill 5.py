e30c8f3375c455e10b69bae7c81f161075d39569
박재우 <wwkkii@naver.com>
2019-10-09 17:07:54 +0900

    캐릭터 좌클릭 이동
    6번째

--- Drill 5/Drill 5.py
+++ Drill 5/Drill 5.py
@@ -7,7 +7,6 @@ running = True
 char_point = [500, 500]
 
 
-
 def handle_events():
     global running
     global x, y
@@ -19,25 +18,29 @@ def handle_events():
             x, y = event.x, KPU_H - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
-        elif event.type == SDL_MOUSEBUTTONDOWN and event.key == SDL_BUTTON_LEFT:
-            p2 = [event.x, event.y]
+        elif event.type == SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_LEFT:
+            p2 = [event.x, KPU_H - 1 - event.y]
             p1 = char_point
             move_character(p1, p2)
+
+frame = 0
+char_state = 3  # 0 왼쪽 달림 1 오른쪽 달림 2 왼쪽 멈춤 3 오른쪽 멈춤
 
 
 def move_character(p1, p2):
     global frame
     for i in range(0, 100 + 1, 5):
+        clear_canvas()
         t = i / 100
         x1 = (1 - t) * p1[0] + t * p2[0]
         y1 = (1 - t) * p1[1] + t * p2[1]
-    e = get_events()
-    frame = (frame + 1) % 8
+        char_point[0] = x1
+        char_point[1] = y1
+        character.clip_draw(frame * 100, 100 * char_state, 100, 100, char_point[0], char_point[1])
+        frame = (frame + 1) % 8
+        update_canvas()
 
 
-frame = 0
-
-char_state = 0  # 0 왼쪽 달림 2 오른쪽 달림 3 왼쪽 멈춤 4 오른쪽 멈춤
 open_canvas(KPU_W, KPU_H)
 kpu_ground = load_image('KPU_GROUND.png')
 mouse = load_image('hand_arrow.png')
