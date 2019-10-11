7e1b378b6ad90b7bb05124f07c5eac8d6b6c61a0
박재우 <wwkkii@naver.com>
2019-10-11 14:55:02 +0900

    곡선 함수 구현

--- Drill 6/Drill 6-2.py
+++ Drill 6/Drill 6-2.py
@@ -1,11 +1,10 @@
 from pico2d import *
+import turtle
 
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
 
-
-
-def handle():
+def stop():
     global running
     events = get_events()
     for event in events:
@@ -15,6 +14,69 @@ def handle():
             running = False
 
 
+def move_3points(p1, p2, p3):
+    global frame
+    for i in range(50, 100, 2):
+        frame += 1
+        t = i / 100
+        x = (2 * t ** 2 - 3 * t + 1) * p1[0] + (-4 * t ** 2 + 4 * t) * p2[0] + (2 * t ** 2 - t) * p3[0]
+        y = (2 * t ** 2 - 3 * t + 1) * p1[1] + (-4 * t ** 2 + 4 * t) * p2[1] + (2 * t ** 2 - t) * p3[1]
+        clear_canvas()
+        Kpu.draw(k_x, k_y)
+        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+        update_canvas()
+        stop()
+
+def move_4point(p1, p2, p3, p4):
+    global frame
+    # draw p1-p2
+    for i in range(0, 100, 2):
+        frame += 1
+        t = i / 100
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p4[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p1[0] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p2[0] + (t ** 3 - t ** 2) * p3[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p4[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p1[1] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p2[1] + (t ** 3 - t ** 2) * p3[1]) / 2
+        clear_canvas()
+        Kpu.draw(k_x, k_y)
+        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+        update_canvas()
+        stop()
+    # draw p2-p3
+    for i in range(0, 100, 2):
+        frame += 1
+        t = i / 100
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p1[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[0] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p3[0] + (t ** 3 - t ** 2) * p4[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p1[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[1] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p3[1] + (t ** 3 - t ** 2) * p4[1]) / 2
+        clear_canvas()
+        Kpu.draw(k_x, k_y)
+        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+        update_canvas()
+        stop()
+    # draw p3-p4
+    for i in range(0, 100, 2):
+        frame += 1
+        t = i / 100
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p2[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p3[0] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p4[0] + (t ** 3 - t ** 2) * p1[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p2[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p3[1] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p4[1] + (t ** 3 - t ** 2) * p1[1]) / 2
+        clear_canvas()
+        Kpu.draw(k_x, k_y)
+        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+        update_canvas()
+        stop()
+
+def move(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10):
+    move_4point(p1, p2, p3, p4)
+    move_4point(p4, p5, p6, p7)
+    move_4point(p7, p8, p9, p10)
+    move_3points(p9, p10, p1)
+
+
+frame = 0
 running = True
 k_x, k_y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
@@ -22,8 +84,5 @@ Kpu = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
 hide_cursor()
 while running:
-    clear_canvas()
-    Kpu.draw(k_x, k_y)
-    update_canvas()
-    handle()
+    
 close_canvas()
