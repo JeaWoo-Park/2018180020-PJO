4a2586bd96ae634d953e65348646de9917dc1802
박재우 <wwkkii@naver.com>
2019-10-14 11:57:19 +0900

    핸들 이벤트
    4번재

--- Drill 7/Drill 7.py
+++ Drill 7/Drill 7.py
@@ -31,3 +31,13 @@ class Boy:
 
     def draw(self):
         self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+
+
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
